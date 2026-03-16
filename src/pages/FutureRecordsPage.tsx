93b00d54f02cef7951dd947b339abc252b6f7de0
bobo <z@bobodeMac-mini.local>
2026-03-16 21:48:03 +0800

    Change FutureRecordsPage transaction type to fund transfer

--- src/pages/FutureRecordsPage.tsx
+++ src/pages/FutureRecordsPage.tsx
@@ -15,7 +15,7 @@ const FutureRecordsPage = () => {
       planName: '友邦強積金優選計劃',
       accountNumber: '56442131',
       icon: './icons/aia-logo-new.jpg',
-      transactionType: '基金轉換',
+      transactionType: '基金轉移',
       source: '流動應用程式',
     },
   ];
